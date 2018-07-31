6b326ffe3374aceb58c8afb0763065d12234d114
spade.magnet <wishborn2004@gmail.com>
2018-07-31 14:14:18 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  include SessionsHelper
+
+  private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order('created_at DESC').page(params[:page])
+    counts(@user)
+  end
+
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
 end
